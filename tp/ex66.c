6c5997f28d06c5bc767913b5394dd5cd69dec35e
Mial <gueddiilyass99@gmail.com>
2025-12-21 15:17:27 +0100

    c

---
+++ tp/ex66.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    int u1 = 1;
+    int u2 = 1;
+    int i = 3
+
+    while (i < 30){
+        un = u
+    }
+
+    return 0;
+}
---
+++ tp/ex77.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int nums[10] = {2,3,4,3,7,5,8,5,8,9};
+
+int main(){
+
+    for i in nums {
+        
+    }
+    return 0;
+}
